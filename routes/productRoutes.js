db1036ed418f02d11f90cffef19124871e55a988
Soumyadip-m46 <designerm46@m46brands.com>
2026-07-06 12:58:38 +0530

    feat: implement cart functionality with user model schema updates and product controller methods

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const multer = require('multer');
-const productController = require('../controllers/productController');
-const { protect, adminOnly } = require('../middlewares/authMiddleware');
+const express = require("express");
+const multer = require("multer");
+const productController = require("../controllers/productController");
+const { protect, adminOnly } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
@@ -12,24 +12,38 @@ const upload = multer({
     fileSize: 10 * 1024 * 1024, // 10MB limit
   },
   fileFilter: (req, file, cb) => {
-    if (file.mimetype.startsWith('image/')) {
+    if (file.mimetype.startsWith("image/")) {
       cb(null, true);
     } else {
-      cb(new Error('Invalid file type. Only images are allowed!'), false);
+      cb(new Error("Invalid file type. Only images are allowed!"), false);
     }
   },
 });
 
 // --- PUBLIC ROUTES ---
 // Available to any customer browsing the website
-router.get('/', productController.getAllProducts);
-router.get('/categories', productController.getProductCategories);
-router.get('/:id', productController.getProductById);
+router.get("/", productController.getAllProducts);
+router.get("/categories", productController.getProductCategories);
+router.get("/:id", productController.getProductById);
 
 // --- PROTECTED ADMIN ROUTES ---
 // You must be logged in AND have an 'admin' role to write/update/delete products
-router.post('/', protect, adminOnly, upload.array('images', 10), productController.createProduct);
-router.put('/:id', protect, adminOnly, upload.array('images', 10), productController.updateProduct);
-router.delete('/:id', protect, adminOnly, productController.deleteProduct);
+router.post(
+  "/",
+  protect,
+  adminOnly,
+  upload.array("images", 10),
+  productController.createProduct,
+);
+router.put(
+  "/:id",
+  protect,
+  adminOnly,
+  upload.array("images", 10),
+  productController.updateProduct,
+);
+router.delete("/:id", protect, adminOnly, productController.deleteProduct);
+router.put("/cart/:id", protect, productController.addToCart);
+router.delete("/cart/:id", protect, productController.removeFromCart);
 
 module.exports = router;
